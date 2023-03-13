7b489e876a30bff6578094d6660c703a42ebdf8b
Torben Evald Hansen <tvupper@gmail.com>
2023-03-13 21:55:17 +0100

    Fixes substraction by possible string

--- src/Http/Controllers/Api/IncidentController.php
+++ src/Http/Controllers/Api/IncidentController.php
@@ -204,7 +204,7 @@ class IncidentController
         list($range, $netmask) = explode('/', $range, 2);
         $range_decimal = ip2long($range);
         $ip_decimal = ip2long($ip);
-        $wildcard_decimal = pow(2, (32 - $netmask)) - 1;
+        $wildcard_decimal = pow(2, (32 - ((int) $netmask))) - 1;
         $netmask_decimal = ~$wildcard_decimal;
 
         return ($ip_decimal & $netmask_decimal) == ($range_decimal & $netmask_decimal);
