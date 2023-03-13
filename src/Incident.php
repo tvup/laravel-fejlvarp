ecfc4e6a80bc81565db9174fa18ccbdfa4443c5a
Torben Evald Hansen <tvupper@gmail.com>
2023-03-13 18:51:02 +0100

    Adds proper doc for Incident-model

--- src/Incident.php
+++ src/Incident.php
@@ -5,6 +5,16 @@ namespace Tvup\LaravelFejlVarp;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string $hash
+ * @property string $subject
+ * @property string $data
+ * @property int $occurrences
+ * @property Carbon $last_seen_at
+ * @property Carbon $resolved_at
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ */
 class Incident extends Model
 {
     use HasFactory;
