a2d2fc20aae8b7e4ee6bc04b952daf500094c7ec
Torben Evald Hansen <tvupper@gmail.com>
2023-03-13 22:41:27 +0100

    Fixes errors of larastan level 6 for IncidentController

--- src/Http/Controllers/IncidentController.php
+++ src/Http/Controllers/IncidentController.php
@@ -3,22 +3,22 @@
 namespace Tvup\LaravelFejlVarp\Http\Controllers;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 use Tvup\LaravelFejlVarp\Incident;
 
 class IncidentController extends \App\Http\Controllers\Controller
 {
-    private mixed $config;
-
     private string $server_name;
 
     public function __construct()
     {
-        $this->config = config('laravelfejlvarp');
-        $this->server_name = ('app.url') . '/api/incidents';
+        $this->server_name = config('app.url') . '/api/incidents';
     }
 
-    public function index(Request $request)
+    public function index(Request $request) : View
     {
         $show_all = false;
         if ($request->boolean('show_all')) {
@@ -29,7 +29,7 @@ class IncidentController extends \App\Http\Controllers\Controller
         return view('laravelfejlvarp::incidents.index', ['show_all' => $show_all, 'incidents' => $incidents, 'user_agent' => '', 'geoip' => '']);
     }
 
-    public function show($hash)
+    public function show(string $hash) : View
     {
         $incident = $this->fejlvarp_find_incident($hash);
         $geoip = null;
@@ -44,7 +44,7 @@ class IncidentController extends \App\Http\Controllers\Controller
         return view('laravelfejlvarp::incidents.show', ['incident' => $incident, 'server_name' => $this->server_name, 'user_agent' => $user_agent, 'geoip' => $geoip]);
     }
 
-    public function destroy($hash)
+    public function destroy(string $hash) : RedirectResponse
     {
         /** @var Incident $incident */
         $incident = Incident::whereHash($hash)->firstOrFail();
@@ -54,24 +54,28 @@ class IncidentController extends \App\Http\Controllers\Controller
         return back();
     }
 
-    public function destroyAll()
+    public function destroyAll() : RedirectResponse
     {
         $this->fejlvarp_prune_old();
 
         return redirect()->route('incidents.index');
     }
 
-    public function fejlvarp_prune_old()
+    private function fejlvarp_prune_old() : void
     {
         Incident::where('last_seen_at', '<', Carbon::now('Europe/Copenhagen')->subDay())->whereNull('resolved_at')->delete();
     }
 
-    public function fejlvarp_find_incident($hash)
+    private function fejlvarp_find_incident(string $hash) : Incident
     {
         return Incident::whereHash($hash)->firstOrFail();
     }
 
-    public function fejlvarp_select_incidents($include_closed = false)
+    /**
+     * @param bool $include_closed
+     * @return Collection<int, Incident>|null
+     */
+    private function fejlvarp_select_incidents(bool $include_closed = false) : ?Collection
     {
         if ($include_closed) {
             return Incident::orderBy('last_seen_at', 'desc')->get();
