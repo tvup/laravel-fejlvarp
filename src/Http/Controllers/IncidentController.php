e74011eca715c5a243d732a556eb101b15de2e1e
Torben Evald Hansen <tvupper@gmail.com>
2023-03-13 18:50:43 +0100

    Removes use of pdo in Controllers/IncidentController

--- src/Http/Controllers/IncidentController.php
+++ src/Http/Controllers/IncidentController.php
@@ -4,12 +4,9 @@ namespace Tvup\LaravelFejlVarp\Http\Controllers;
 
 use Tvup\LaravelFejlVarp\Incident;
 use Illuminate\Http\Request;
-use PDO;
 
 class IncidentController extends \App\Http\Controllers\Controller
 {
-    private pdo $db;
-
     private mixed $config;
 
     private string $server_name;
@@ -18,9 +15,6 @@ class IncidentController extends \App\Http\Controllers\Controller
     {
         $this->config = config('laravelfejlvarp');
         $this->server_name = ('app.url') . '/api/incidents';
-        $dsn = 'mysql:host=' . config('database.connections.app_api_no_prefix.host') . (config('database.connections.app_api_no_prefix.port') ? ':' . config('database.connections.app_api_no_prefix.port') : '') . ';dbname=' . config('database.connections.app_api_no_prefix.database');
-        $this->db = new PDO($dsn, config('database.connections.app_api_no_prefix.username'), config('database.connections.app_api_no_prefix.password'));
-        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
     public function index(Request $request)
@@ -51,12 +45,9 @@ class IncidentController extends \App\Http\Controllers\Controller
 
     public function destroy($hash)
     {
-        $q = $this->db->prepare('UPDATE incidents SET resolved_at = NOW() WHERE hash = :hash');
-        $q->execute(
-            [
-                ':hash' => $hash]
-        );
-
+        /** @var Incident $incident */
+        $incident = Incident::whereHash($hash)->firstOrFail();
+        $incident->resolved_at = Carbon::now('Europe/Copenhagen');
         return back();
     }
 
@@ -69,21 +60,12 @@ class IncidentController extends \App\Http\Controllers\Controller
 
     public function fejlvarp_prune_old()
     {
-        $this->db->exec('UPDATE incidents SET resolved_at = NOW() WHERE last_seen_at < DATE_ADD(NOW(), INTERVAL -1 DAY) and resolved_at is null');
+        Incident::where('last_seen_at', '<', Carbon::now('Europe/Copenhagen')->subDay())->whereNull('resolved_at')->delete();
     }
 
     public function fejlvarp_find_incident($hash)
     {
-        Incident::whereHash($hash)->firstOrFail();
-        $q = $this->db->prepare('SELECT * FROM incidents WHERE hash = :hash');
-        $q->execute([':hash' => $hash]);
-        $row = $q->fetch(PDO::FETCH_ASSOC);
-        $decoded = json_decode($row['data'], true);
-        if ($decoded) {
-            $row['data'] = $decoded;
-        }
-
-        return $row;
+        return Incident::whereHash($hash)->firstOrFail();
     }
 
     public function fejlvarp_select_incidents($include_closed = false)
