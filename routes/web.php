490ba8ddef9c525cb3acb6e6020bdd9846de4200
Torben Evald Hansen <contact@torbenit.dk>
2023-09-04 05:22:02 +0200

    Refactor middleware in web routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::middleware(['admin','auth'])->get('incidents', 'Tvup\LaravelFejlvarp\Http\Controllers\IncidentController@index')->name('incidents.index');
-Route::middleware(['admin','auth'])->get('incidents/{hash}', 'Tvup\LaravelFejlvarp\Http\Controllers\IncidentController@show')->name('incident.show');
-Route::middleware(['admin','auth'])->post('incidents/{hash}/delete', 'Tvup\LaravelFejlvarp\Http\Controllers\IncidentController@destroy')->name('incident.delete');
-Route::middleware(['admin','auth'])->post('incidents/delete', 'Tvup\LaravelFejlvarp\Http\Controllers\IncidentController@destroyAll')->name('incidents.delete');
+Route::middleware(['admin'])->get('incidents', 'Tvup\LaravelFejlvarp\Http\Controllers\IncidentController@index')->name('incidents.index');
+Route::middleware(['admin'])->get('incidents/{hash}', 'Tvup\LaravelFejlvarp\Http\Controllers\IncidentController@show')->name('incident.show');
+Route::middleware(['admin'])->post('incidents/{hash}/delete', 'Tvup\LaravelFejlvarp\Http\Controllers\IncidentController@destroy')->name('incident.delete');
+Route::middleware(['admin'])->post('incidents/delete', 'Tvup\LaravelFejlvarp\Http\Controllers\IncidentController@destroyAll')->name('incidents.delete');
