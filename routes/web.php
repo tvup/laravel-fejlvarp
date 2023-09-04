e35b940563b8df5f25cbd5b887d7f99fa74acfc9
Torben Evald Hansen <contact@torbenit.dk>
2023-09-04 04:53:56 +0200

    Refactor routes to include authentication middleware

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,6 @@
 <?php
 
-Route::get('incidents', 'Tvup\LaravelFejlvarp\Http\Controllers\IncidentController@index')->name('incidents.index');
-Route::get('incidents/{hash}', 'Tvup\LaravelFejlvarp\Http\Controllers\IncidentController@show')->name('incident.show');
-Route::post('incidents/{hash}/delete', 'Tvup\LaravelFejlvarp\Http\Controllers\IncidentController@destroy')->name('incident.delete');
-Route::post('incidents/delete', 'Tvup\LaravelFejlvarp\Http\Controllers\IncidentController@destroyAll')->name('incidents.delete');
-
-
-
-
-
-
-
+Route::middleware(['admin','auth'])->get('incidents', 'Tvup\LaravelFejlvarp\Http\Controllers\IncidentController@index')->name('incidents.index');
+Route::middleware(['admin','auth'])->get('incidents/{hash}', 'Tvup\LaravelFejlvarp\Http\Controllers\IncidentController@show')->name('incident.show');
+Route::middleware(['admin','auth'])->post('incidents/{hash}/delete', 'Tvup\LaravelFejlvarp\Http\Controllers\IncidentController@destroy')->name('incident.delete');
+Route::middleware(['admin','auth'])->post('incidents/delete', 'Tvup\LaravelFejlvarp\Http\Controllers\IncidentController@destroyAll')->name('incidents.delete');
