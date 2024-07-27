a2425e633f0441c8ae9de819e7fe0cab098e5fdc
Torben Evald Hansen <contact@torbenit.dk>
2024-07-27 13:14:31 +0200

    Update PHP CS Fixer and add IncidentFactory

--- src/Http/Requests/IncidentStoreRequest.php
+++ src/Http/Requests/IncidentStoreRequest.php
@@ -7,7 +7,7 @@ class IncidentStoreRequest extends ApiRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array
+     * @return array<string, string>
      */
     public function rules()
     {
