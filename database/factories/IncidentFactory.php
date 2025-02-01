dc6d17b4acf9505a4c2c54948e84a18ab2d8f3f8
Torben Evald Hansen <tvupper@gmail.com>
2025-02-01 04:47:54 +0100

    Add PHP 8.4 support and improve test coverage (#47)
    * Migrate config renovate.json (#46)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Add PHP 8.4 support and improve test coverage
    
    * Fix content-type of geostackip-script
    
    * Add Guzzle to require-dev for HTTP client testing
    
    * Update orchestra/testbench to version constraint to ^9.1
    
    * Refactor PHP extensions for streamlined setup
    
    * Remove unused ext-pcntl dependency
    
    * Add SQLite extension for testing environment
    
    * Remove unused curl extension from dependencies
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- database/factories/IncidentFactory.php
+++ database/factories/IncidentFactory.php
@@ -6,8 +6,9 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Carbon;
 use Tvup\LaravelFejlvarp\Incident;
 
+
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<Incident>
+ * @extends Factory<Incident>
  */
 class IncidentFactory extends Factory
 {
@@ -15,10 +16,13 @@ class IncidentFactory extends Factory
 
     public function definition(): array
     {
+        $fixturePath = __DIR__ . '/../../tests/Fixtures/incident_data_command_not_found.json';
+        $incidentData = file_get_contents($fixturePath);
+
         return [
             'hash' => $this->faker->uuid(),
             'subject' => $this->faker->sentence(),
-            'data' => $this->faker->optional()->randomElements(),
+            'data' => $this->faker->randomElements([$incidentData]),
             'occurrences' => $this->faker->numberBetween(1, 100),
             'last_seen_at' => Carbon::instance($this->faker->dateTime()),
             'resolved_at' => null,
